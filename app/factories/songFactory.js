8dbb5091b4a59501cff96c9c5a6f9b73e6b279ef
Wisbell <westleyisbell@gmail.com>
2017-01-31 08:58:13 -0600

    Deleted 2 unnecessary files.  Added blue print for 2 new functions in SongFactory.js

--- app/factories/songFactory.js
+++ app/factories/songFactory.js
@@ -12,5 +12,21 @@ app.factory('SongFactory', function($http){
             addNewSong: function(newSong){
               $http.post('https://west-music-history.firebaseio.com/.json', JSON.stringify(newSong))
             }
-          }
+          },
+
+            getArtistList: function(){
+              return $http.get('https://west-music-history.firebaseio.com/.json')
+                .then(function(val){
+                  console.log("val", val)
+                  return val.data
+              })
+            },
+
+            getAlbumList: function(){
+              return $http.get('https://west-music-history.firebaseio.com/.json')
+                .then(function(val){
+                  console.log("val", val)
+                  return val.data
+              })
+            }
 })
