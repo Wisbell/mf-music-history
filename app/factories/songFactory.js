550c1ae8c957391adb6dd7b71e06f12a962b9cfa
Wisbell <westleyisbell@gmail.com>
2017-01-31 09:41:42 -0600

    Working on ablility to add artists to options list

--- app/factories/songFactory.js
+++ app/factories/songFactory.js
@@ -11,14 +11,33 @@ app.factory('SongFactory', function($http){
 
             addNewSong: function(newSong){
               $http.post('https://west-music-history.firebaseio.com/.json', JSON.stringify(newSong))
-            }
-          },
+            },
 
             getArtistList: function(){
               return $http.get('https://west-music-history.firebaseio.com/.json')
                 .then(function(val){
-                  console.log("val", val)
+                  console.log("get artist list val", val)
+
+
+                  // get artist info
+
+                  // add it to an array
+
+                  // ng-repeat it as an option
                   return val.data
+              })
+              .then(function(val){
+                console.log("second then", val)
+
+                 let artistList = [];
+                  // go over object list
+
+                  for (key in val.data) {
+                    console.log("get artist", key.artist)
+                    artistList.push(key.artist)
+                  }
+
+                  console.log(artistList)
               })
             },
 
@@ -29,4 +48,5 @@ app.factory('SongFactory', function($http){
                   return val.data
               })
             }
+          }
 })
